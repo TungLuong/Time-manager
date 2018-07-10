b4395e858696443d31ece2ea60eba76e67290890
TungLuong <35015720+TungLuong@users.noreply.github.com>
2018-07-10 10:05:30 +0700

    update

--- app/src/main/java/tl/com/timemanager/Base/BaseFragment.java
+++ app/src/main/java/tl/com/timemanager/Base/BaseFragment.java
@@ -1,11 +1,34 @@
 package tl.com.timemanager.Base;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 
 public class BaseFragment extends Fragment {
     public void onBackPressed() {
-        // do nothing
-        //((BaseActivity)getActivity()).onBackRoot();
+       showDialogDelete();
+    }
+    private void showDialogDelete() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Thoát");
+        builder.setMessage("Bạn có chắc chắn muốn thoát không");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Quay lại", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setNegativeButton("Đồng ý", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                ((BaseActivity)getActivity()).onBackRoot();
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
 
     }
 }
